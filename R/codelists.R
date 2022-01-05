865deef5b3d071b3b15fbcff5bd2804d2a6c78ca
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-01-05 09:50:10 +0000

    Adding in a mock function

---
+++ R/codelists.R
@@ -0,0 +1,13 @@
+
+#' Apply codelists
+#'
+#' @param dataset dataset to apply the codelist to
+#' @param metacore_slice filtered metacore object to only have the dataset you are looking at
+#'
+#' @return
+#' @export
+#'
+#' @examples
+apply_codelist <- function(dataset, metacore_slice){
+   print("hello world")
+}
