0a7f9f63ee2acb20efa94d5d770db0ce4c5927ed
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-01-20 13:43:48 +0000

    Adding in tests for sorting

---
+++ tests/testthat/test-sort.R
@@ -0,0 +1,18 @@
+library(haven)
+
+spec <- define_to_metacore(metacore_example("ADaM_define.xml"), quiet = TRUE) %>%
+   select_dataset("ADSL")
+data <- read_xpt(metatools_example("adsl.xpt"))
+test_that("sort_order", {
+   data %>%
+      select(AGE, SITEID, everything()) %>%
+      sort_order(spec) %>%
+      expect_equal(data)
+})
+
+test_that("sort_key", {
+   data %>%
+      arrange(TRT01P, AGE) %>%
+      sort_key(spec) %>%
+      expect_equal(data)
+})
