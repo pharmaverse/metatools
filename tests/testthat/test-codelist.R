92e8b74476120286dc93b0d5defee46cd1cfdc1b
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-02-09 15:41:10 +0000

    #9 Adding tests for converting to num when num CT

--- tests/testthat/test-codelist.R
+++ tests/testthat/test-codelist.R
@@ -2,6 +2,14 @@ library(haven)
 library(tibble)
 library(dplyr)
 
+
+
+spec <- spec_to_metacore(metacore_example("p21_mock.xlsx"), quiet = TRUE)
+load(metacore_example("pilot_ADaM.rda"))
+spec2 <- metacore %>% select_dataset("ADSL")
+dm <- read_xpt(metatools_example("dm.xpt"))
+
+
 test_that("create_subgrps", {
   expect_equal(
     create_subgrps(c(1:10), c("<2", "2-5", ">5")),
@@ -34,7 +42,6 @@ test_that("create_var_from_codelist", {
     4, "U", "Unknown",
     5, "M", "Male",
   )
-  spec <- spec_to_metacore(metacore_example("p21_mock.xlsx"), quiet = TRUE)
   manual_data <- data %>%
     mutate(SEX = VAR1)
   expect_equal(create_var_from_codelist(data, spec, VAR2, SEX), manual_data)
@@ -45,14 +52,20 @@ test_that("create_var_from_codelist", {
     create_var_from_codelist(data, spec, VAR1, SEX, decode_to_code = FALSE),
     manual_data2
   )
+  # Test numerics
+  num_out <- dm %>%
+     mutate(TRT01P = ARM) %>%
+     select(TRT01P) %>%
+     create_var_from_codelist(spec2, TRT01P, TRT01PN) %>%
+     head() %>%
+     pull(TRT01PN)
+  expect_equal(num_out, c(0,  0, 81, 54, 81,0))
+
   # Test for Variable not in specs
   expect_error(create_var_from_codelist(data, spec, VAR2, FOO))
 })
 
 test_that("create_cat_var", {
-  spec <- define_to_metacore(metacore_example("ADaM_define.xml"), quiet = TRUE) %>%
-    select_dataset("ADSL")
-  dm <- read_xpt(metatools_example("dm.xpt"))
   # Create manual dataset to check against
   man_dat <- tribble(
     ~AGEGR1,     ~n,
@@ -61,12 +74,12 @@ test_that("create_cat_var", {
     ">80",       92,
   )
   # Grouping col only
-  auto_dat <- create_cat_var(dm, spec, AGE, AGEGR1) %>%
+  auto_dat <- create_cat_var(dm, spec2, AGE, AGEGR1) %>%
     group_by(AGEGR1) %>%
     summarise(n = n())
   expect_equal(auto_dat, man_dat)
   # Grouping Column and Numeric Decode
-  grp_num_dat <- create_cat_var(dm, spec, AGE, AGEGR1, AGEGR1N)
+  grp_num_dat <- create_cat_var(dm, spec2, AGE, AGEGR1, AGEGR1N)
   grp_num_dat %>%
     group_by(AGEGR1) %>%
     summarise(n = n()) %>%
@@ -76,15 +89,10 @@ test_that("create_cat_var", {
     unique() %>%
     expect_equal(as.character(1:3))
   # Test errors
-  spec <- spec_to_metacore(metacore_example("p21_mock.xlsx"), quiet = TRUE)
   expect_error(create_cat_var(dm, spec, AGE, ARM))
 })
 
 test_that("convert_var_to_fct", {
-  spec <- spec_to_metacore(metacore_example("p21_mock.xlsx"), quiet = TRUE)
-  foo <- spec$codelist
-  foo2 <- spec$value_spec
-  dm <- read_xpt(metatools_example("dm.xpt"))
   # Codelist variable
   convert_var_to_fct(dm, spec, SEX) %>%
     pull(SEX) %>%
