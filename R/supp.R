03a0fcedf69409491725857e3b9129fd3903b8fc
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-02-24 14:35:14 +0000

    Adding combine_supp function and tests

--- R/supp.R
+++ R/supp.R
@@ -96,12 +96,83 @@ make_supp_qual <- function(dataset, metacore, idvar = NULL, dataset_name = NULL)
              qorig = .data$origin, qeval = .data$derivation) %>%
       mutate(idvar = idvar_str) %>%
       distinct() #Protection against bad specs
-
+   #TODO Addin in checks/coerision for when combining cols of different types
    pmap_dfr(supp_meta, build_qnam, dataset=dataset) %>%
       arrange(.data$USUBJID, .data$QNAM, .data$IDVARVAL)
 }
 
-# make_supp_qual(ae, metacore)
+#' Combine the Domain and Supplemental Qualifier
+#'
+#' @param dataset Domain dataset
+#' @param supp Supplemental Qualifier dataset
+#'
+#' @return
+#' @export
+#'
+#' @importFrom purrr discard map reduce
+#' @importFrom dplyr if_else select group_by group_split pull rename left_join
+#' @importFrom tidyr pivot_wider
+#' @importFrom rlang sym
+#'
+#' @examples
+#' library(safetyData)
+#' combine_supp(sdtm_ae, sdtm_suppae)
+combine_supp <- function(dataset, supp){
+   if(!is.data.frame(dataset) | !is.data.frame(supp)){
+      stop("You must supply a domain and supplemental dataset", call. = FALSE)
+   }
+   supp_cols <- c("STUDYID", "RDOMAIN", "USUBJID", "IDVAR", "IDVARVAL",
+                  "QNAM", "QLABEL", "QVAL", "QORIG", "QEVAL")
+   ext_supp_col <- names(supp) %>%  discard(~. %in% supp_cols)
+   mis_supp_col <- supp_cols %>%  discard(~. %in% names(supp))
+   if(length(ext_supp_col) > 0 | length(mis_supp_col) > 0){
+      mess <- "Supplemental datasets need to comply with CDISC standards\n"
+      ext <- if_else(length(ext_supp_col) > 0,
+                     paste0("The following columns need to be removed:\n", paste0(ext_supp_col, collapse = "\n")),
+                     "")
+      mis <- if_else(length(mis_supp_col) > 0,
+                     paste0("The following columns are missing:\n", paste0(mis_supp_col, collapse = "\n")),
+                     "")
+      stop(paste0(mess, ext, mis))
+   }
+   by <- names(dataset) %>%
+      discard(~ . %in% supp$QNAM) # Don't want any variables in our by statement
 
+   # In order to prevent issues when there are multiple IDVARS we need to merge
+   # each IDVAR into the domain seperately (otherwise there is problems when the
+   # two IDVARS don't overlap)
+   supp %>%
+      select(-.data$QLABEL, -.data$QORIG, -.data$QEVAL) %>% #Removing columns not for the main dataset
+      rename(DOMAIN = .data$RDOMAIN) %>%
+      group_by(.data$IDVAR) %>% #For when there are multiple IDs
+      group_split() %>%
+      map(function(x) {
+         # Get the IDVAR value to allow for renaming of IDVARVAL
+         id_var <- x %>%
+            pull(.data$IDVAR) %>%
+            unique()
+         wide_x <- x %>%
+            pivot_wider(
+               names_from = .data$QNAM,
+               values_from = .data$QVAL) %>%
+            select(-.data$IDVAR)
+         if(!is.na(id_var) && id_var  != ""){
+            wide_x <- wide_x %>%
+               rename(!!sym(id_var) := .data$IDVARVAL) #Given there is only one ID per df we can just rename
 
+            by <- c("STUDYID", "DOMAIN", "USUBJID", id_var)
+
+            out <- left_join(dataset, wide_x,
+                      by = by)
+         } else {
+            wide_x <- wide_x %>%
+               select(-.data$IDVARVAL)
+            out <- left_join(dataset, wide_x,
+                             by = c("STUDYID", "DOMAIN", "USUBJID"))
+         }
+         out
+      }) %>%
+      reduce(full_join, by= by)
+
+}
 
--- tests/testthat/test-supp.R
+++ tests/testthat/test-supp.R
@@ -111,6 +111,7 @@ test_that("make_supp_qual", {
    })
    #Testing normal circumstances
    expect_equal(metacore_supp, man_supp)
+
    #Testing with quotes
    metacore_supp <- make_supp_qual(ae, spec, "AESEQ") %>%
       arrange(USUBJID, QNAM, IDVARVAL) %>%
@@ -127,9 +128,78 @@ test_that("make_supp_qual", {
 })
 
 
+test_that("combine_supp", {
+   ### 1 IDVAR and 1 QNAM
+   combo_ae <- combine_supp(sdtm_ae, sdtm_suppae) %>%
+      select(USUBJID, AESEQ, AETRTEM) %>%
+      distinct() %>%
+      arrange(USUBJID, AESEQ)
+   supp_check <- sdtm_suppae %>%
+      select(USUBJID, AESEQ = IDVARVAL, AETRTEM = QVAL) %>%
+      arrange(USUBJID, AESEQ)
+   expect_equal(combo_ae, supp_check)
 
+   ### No IDVAR and multiple QNAM
+   out_test <- sdtm_suppdm %>%
+      filter(USUBJID %in% c("01-701-1015")) %>%
+      select(USUBJID, QNAM, QVAL) %>%
+      pivot_wider(names_from = QNAM, values_from = QVAL) %>%
+      as.data.frame()
 
+   full_dm <- combine_supp(sdtm_dm, sdtm_suppdm) %>%
+      select(USUBJID, COMPLT16:SAFETY)
+   expect_equal(filter(full_dm, USUBJID == "01-701-1015"), out_test)
+   # Test SUBJID that wasn't in the SUPP that all supp values are NA
+   full_dm %>%
+      filter(USUBJID == "01-701-1057") %>%
+      select(-USUBJID) %>%
+      pivot_longer(everything())%>%
+      summarise(test = all(is.na(value))) %>%
+      expect_equal(tibble(test = TRUE))
 
+   ### Where there are only value for a small number of subjects
+   mostly_miss <- combine_supp(sdtm_ds, sdtm_suppds)
+   original <- sdtm_suppds %>%
+      arrange(USUBJID) %>%
+      pull(QVAL)
+   expect_equal(mostly_miss %>%
+                      filter(!is.na(ENTCRIT)) %>%
+                      arrange(USUBJID) %>%
+                      pull(ENTCRIT),
+                   original)
 
+   ### Multiple IDVARS and multiple QNAMS
+   # Add some mock supp variables
+   ae <- sdtm_ae %>%
+      mutate(
+         SUPPVAR1 = words[1:nrow(sdtm_ae)],
+         SUPPVAR2 = rep(letters, 36)[1:nrow(sdtm_ae)],
+         SUPPVAR3 = USUBJID,
+         IDVAR = as.numeric(str_extract(USUBJID, "\\d{3}$"))
+      )
+   ### Mock up a metadata necessary to make the SUPP
+   supp_meta <- tibble::tribble(
+      ~qnam, ~qlabel, ~idvar, ~qeval, ~qorig,
+      "SUPPVAR1", "Supp Test 1", "AESEQ", "Investigator", "CRF",
+      "SUPPVAR2", "Supp Test 2", "AESEQ", "Investigator", "CRF",
+      "SUPPVAR3", "Supp Test 3", "IDVAR", "Investigator", "CRF",
+   )
+
+   ### Wrap and map
+   suppae <- pmap_dfr(supp_meta, build_qnam, dataset=ae) %>%
+      arrange(USUBJID, QNAM, IDVARVAL)
+
+   dataset = ae %>%
+      select(-starts_with("SUPP"))
+   supp = suppae
+   multi_out <- combine_supp(ae, suppae) %>%
+      summarise(v1 = all(all.equal(SUPPVAR1.x, SUPPVAR1.y)), #Because there are NA rows
+                v2 = all(all.equal(SUPPVAR2.x, SUPPVAR2.y)),
+                v3 = all(SUPPVAR3.x == SUPPVAR3.y)) %>%
+      pivot_longer(everything()) %>%
+      pull(value) %>%
+      all()
+   expect_equal(multi_out, TRUE)
+})
 
 
