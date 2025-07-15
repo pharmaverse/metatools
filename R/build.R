69b29272246fe611365083a77d4284994587210f
Liam Hobby <liam.f.hobby+gsk@gsk.com>
2025-07-15 18:50:14 +0100

    Remove call to handler

--- R/build.R
+++ R/build.R
@@ -345,7 +345,13 @@ drop_unspec_vars <- function(dataset, metacore, dataset_name = deprecated()) {
 #' add_variables(data, spec)
 add_variables <- function(dataset, metacore, dataset_name = deprecated()){
    if (is_present(dataset_name)) {
-      handle_deprecate_dataset_name("add_variables(dataset_name)")
+      lifecycle::deprecate_warn(
+         when = "0.2.0",
+         what = "add_variables(dataset_name)",
+         details = cli_inform(c("i" = col_red("The {.arg dataset_name} argument will be removed in a future release.
+      Please use {.fn metacore::select_dataset} to subset the {.obj metacore} object to obtain
+      metadata for a single dataset.")))
+      )
       metacore <- make_lone_dataset(metacore, dataset_name)
    }
 
